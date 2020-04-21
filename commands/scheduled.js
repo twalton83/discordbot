7b238905880b6ae33b3bc747e4153c32c6f2b45f
Tatiana <tatianaw83@gmail.com>
2020-04-21 08:45:18 -0700

    updated cron jobs

--- commands/scheduled.js
+++ commands/scheduled.js
@@ -2,7 +2,7 @@ const cron = require('cron');
 const {checkWeather} = require('./weather')
 
 let goodMorning = new cron.CronJob('0 * 8 * * * ', () =>{
-    myChannel.send(`Good morning! It is 8:00am, currently ${currentTemp} degrees Farenheit. Happy ${currentDate}!`)
+   checkWeather()
 });
 
 let goodNoon = new cron.CronJob('0 * 12 * * * ', () =>{
